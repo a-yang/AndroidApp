6f4e44a2590a55a5931e65e9d2bfd562fd03f0c9
a-yang <angela.yang316@gmail.com>
2016-04-09 01:10:04 -0400

    Search page -> purchase page working with toylistgit status (price missing)

--- app/src/main/java/com/example/abigailwatson/project3/SearchActivity.java
+++ app/src/main/java/com/example/abigailwatson/project3/SearchActivity.java
@@ -13,7 +13,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import android.widget.ListView;
-import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
 import android.view.DragEvent;
 import android.widget.FrameLayout;
@@ -21,12 +20,9 @@ import android.content.ClipData;
 import android.content.ClipDescription;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.view.View.DragShadowBuilder;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Point;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-
+import android.content.Intent;
+import android.app.ProgressDialog;
+import android.widget.ArrayAdapter;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -36,11 +32,8 @@ public class SearchActivity extends AppCompatActivity {
     private  FrameLayout targetLayout;
     private ListView lv;
     private android.widget.FrameLayout.LayoutParams layoutParams;
-    private ArrayList<String> names;
+    private ArrayList<String> names = new ArrayList<String>();
 
-    TextView comments;
-
-    String commentMsg;
 
     private ToyList shoppingCart = new ToyList();
     private ArrayList<String> fakeShoppingCart = new ArrayList<String>();
@@ -49,39 +42,23 @@ public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        names = new ArrayList<String>();
-        names.add("Angela");
-        names.add("Abby");
-        names.add("Izzy");
-        names.add("Adam");
-        names.add("Mannhi");
-        names.add("Kevin");
-        names.add("Jenn");
-        names.add("Joseph");
-        names.add("Annette");
-        names.add("Tony");
-        names.add("Agnes");
 
         super.onCreate(savedInstanceState);
 
-        new InternetClass().execute(textSource);
+        new InternetClass(this).execute(textSource);
+
+        names.add("Angela");
+        names.add("Mannhi");
+        names.add("Jenn");
+        names.add("Izzy");
+        names.add("Annette");
+        names.add("Vicky");
 
         setContentView(R.layout.activity_search);
 
         lv = (ListView) findViewById(R.id.list_of_toys);
 
         targetLayout = (FrameLayout)findViewById(R.id.targetlayout);
-        comments = (TextView)findViewById(R.id.comments);
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                names);
-
-        lv.setAdapter(arrayAdapter);
-        lv.setOnItemLongClickListener(listSourceItemLongClickListener);
-        lv.setOnDragListener(myDragEventListener);
-        targetLayout.setOnDragListener(myDragEventListener);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -97,46 +74,21 @@ public class SearchActivity extends AppCompatActivity {
                                        int position, long id) {
 
             //Selected item is passed as item in dragData
-            ClipData.Item item = new ClipData.Item(names.get(position));
+            ClipData.Item item = new ClipData.Item(Integer.toString(position));
 
             String[] clipDescription = {ClipDescription.MIMETYPE_TEXT_PLAIN};
             ClipData dragData = new ClipData((CharSequence) v.getTag(),
                     clipDescription,
                     item);
-            DragShadowBuilder myShadow = new MyDragShadowBuilder(v);
+            DragShadowBuilder myShadow = new DragShadowBuilder(v);
 
             v.startDrag(dragData, //ClipData
                     myShadow,  //View.DragShadowBuilder
-                    names.get(position),  //Object myLocalState
+                    buttons.getToyList().get(position),  //Object myLocalState
                     0);    //flags
 
             return true;
         }};
-
-    private static class MyDragShadowBuilder extends View.DragShadowBuilder {
-        private static Drawable shadow;
-
-        public MyDragShadowBuilder(View v) {
-            super(v);
-            shadow = new ColorDrawable(Color.LTGRAY);
-        }
-
-        @Override
-        public void onProvideShadowMetrics (Point size, Point touch){
-            int width = getView().getWidth();
-            int height = getView().getHeight();
-
-            shadow.setBounds(0, 0, width, height);
-            size.set(width, height);
-            touch.set(width / 2, height / 2);
-        }
-
-        @Override
-        public void onDrawShadow(Canvas canvas) {
-            shadow.draw(canvas);
-        }
-
-    }
 
     public void goToScreenOne(View view) {
         finish();
@@ -159,46 +111,24 @@ public class SearchActivity extends AppCompatActivity {
                     if (event.getClipDescription()
                             .hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN))
                     {
-                        commentMsg = v.getTag()
-                                + " : ACTION_DRAG_STARTED accepted.\n";
-                        comments.setText(commentMsg);
                         return true; //Accept
                     }else{
-                        commentMsg = v.getTag()
-                                + " : ACTION_DRAG_STARTED rejected.\n";
-                        comments.setText(commentMsg);
                         return false; //reject
                     }
                 case DragEvent.ACTION_DRAG_ENTERED:
-                    commentMsg = v.getTag() + " : ACTION_DRAG_ENTERED.\n";
-                    comments.setText(commentMsg);
                     return true;
                 case DragEvent.ACTION_DRAG_LOCATION:
-                    commentMsg = v.getTag() + " : ACTION_DRAG_LOCATION - "
-                            + event.getX() + " : " + event.getY() + "\n";
-                    comments.setText(commentMsg);
                     return true;
                 case DragEvent.ACTION_DRAG_EXITED:
-                    commentMsg = v.getTag() + " : ACTION_DRAG_EXITED.\n";
-                    comments.setText(commentMsg);
                     return true;
                 case DragEvent.ACTION_DROP:
                     // Gets the item containing the dragged data
                     ClipData.Item item = event.getClipData().getItemAt(0);
 
-                    commentMsg = v.getTag() + " : ACTION_DROP" + "\n";
-                    comments.setText(commentMsg);
-
                     //If apply only if drop on buttonTarget
                     if(v == targetLayout){
-                        String droppedItem = item.getText().toString();
-                        Toy oneToy = new Toy();
-                        oneToy.toyName = droppedItem;
-                        shoppingCart.addToy(oneToy);
-                        commentMsg = "Dropped item - "
-                                + droppedItem + "\n";
-                        comments.setText(commentMsg);
-
+                        String droppedItemIndex = item.getText().toString();
+                        shoppingCart.addToy(buttons.getToyList().get(Integer.parseInt(droppedItemIndex)));
 
                         return true;
                     }else{
@@ -207,28 +137,21 @@ public class SearchActivity extends AppCompatActivity {
 
 
                 case DragEvent.ACTION_DRAG_ENDED:
-                    if (event.getResult()){
-                        commentMsg = v.getTag() + " : ACTION_DRAG_ENDED - success.\n";
-                        comments.setText(commentMsg);
-                    } else {
-                        commentMsg = v.getTag() + " : ACTION_DRAG_ENDED - fail.\n";
-                        comments.setText(commentMsg);
-                    };
                     return true;
                 default: //unknown case
-                    commentMsg = v.getTag() + " : UNKNOWN !!!\n";
-                    comments.setText(commentMsg);
                     return false;
 
             }
         }
     }
 
-
-
-
-
     private class InternetClass extends AsyncTask<String, Void, ToyList> {
+
+        private ProgressDialog dialog;
+
+        public InternetClass(SearchActivity activity) {
+            dialog = new ProgressDialog(activity);
+        }
 
 
         @Override
@@ -306,8 +229,26 @@ public class SearchActivity extends AppCompatActivity {
 
         }
 
+        @Override
+        protected void onPreExecute() {
+            dialog.setMessage("Downloading toys...");
+            dialog.show();
+        }
+
         protected void onPostExecute(ToyList toyList) {
             buttons = toyList;
+
+            ToyListAdapter arrayAdapter = new ToyListAdapter(SearchActivity.this, buttons);
+            //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+            lv.setAdapter(arrayAdapter);
+            lv.setOnItemLongClickListener(listSourceItemLongClickListener);
+            lv.setOnDragListener(myDragEventListener);
+            targetLayout.setOnDragListener(myDragEventListener);
+
+
+            if (dialog.isShowing()) {
+                dialog.dismiss();
+            }
         }
 
     /*
---
+++ app/src/main/java/com/example/abigailwatson/project3/toyListAdapter.java
@@ -0,0 +1,67 @@
+package com.example.abigailwatson.project3;
+
+/**
+ * Created by angelayang316 on 4/8/16.
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ToyListAdapter extends BaseAdapter {
+
+    ToyList toyList;
+    Context context;
+
+    private static LayoutInflater inflater=null;
+
+    public ToyListAdapter(SearchActivity searchActivity, ToyList tl) {
+        // TODO Auto-generated constructor stub
+        toyList = tl;
+        context=searchActivity;
+        inflater = ( LayoutInflater )context.
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return toyList.getToyList().size();
+    }
+
+    @Override
+    public Toy getItem(int position) {
+        // TODO Auto-generated method stub
+        return toyList.getToy(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    public class Holder
+    {
+        TextView name;
+        TextView price;
+        ImageView img;
+    }
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        Holder holder=new Holder();
+        View rowView;
+        rowView = inflater.inflate(R.layout.toy_listview, null);
+        holder.name=(TextView) rowView.findViewById(R.id.toyName);
+        holder.img=(ImageView) rowView.findViewById(R.id.toyImage);
+        holder.name.setText(toyList.getToy(position).getToyName());
+        holder.img.setImageBitmap(toyList.getToy(position).getImage());
+
+        return rowView;
+    }
+}
