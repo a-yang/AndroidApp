6f0b2873cad06226e7717d2e431e0d1b33d643cb
a-yang <angela.yang316@gmail.com>
2016-04-10 21:36:51 -0400

    added goToScreenTwo for Cancel Purchase button in Purchase Activity

--- app/src/main/java/com/example/abigailwatson/project3/PurchaseActivity.java
+++ app/src/main/java/com/example/abigailwatson/project3/PurchaseActivity.java
@@ -89,4 +89,10 @@ public class PurchaseActivity extends AppCompatActivity {
         startActivity(launchBrowser);
 
     }
+
+    public void goToScreenTwo(View view) {
+        purchases.getToyList().clear();
+        finish();
+    }
+
 }
